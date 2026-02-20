b91879e3edbdfdce643ea887b2dc91ad8ff93b14
Vidhi Nagar <vidhinagr6644@gmail.com>
2026-02-20 21:17:31 +0530

    Updated frontend URL

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const newRequest = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://freelancer-marketplace-server-side.vercel.app",
+    "https://freelancer-marketplace-server-side.vercel.app/api",
   withCredentials: true,
 });
 
